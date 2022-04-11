6899aab5d01c51c49505d38268f6080bd8533a19
RotemShezaf <rotem.shezaf@gmail.com>
2022-04-11 21:04:53 +0300

    worksTypeDefWithMain

--- RLEList.c
+++ RLEList.c
@@ -1,21 +1,27 @@
 #include "RLEList.h"
+#include <stdlib.h>
 
-typedef struct list {
+typedef struct  RLEList_t {
     char data;
-    char* next;
+    struct RLEList_t * next;
     int multiplier;
     //TODO: implement
 } RLEList_t;
 
 
-RLEList RLEListCreate() {
 
-
-};
 
 void RLEListDestroy(RLEList list) {
 
+
 };
 
-
+int main() {
+    RLEList list;
+    list = (RLEList)malloc(sizeof(RLEList_t));
+    list->data = 'c';
+    printf("%c", list->data);
+    free(list);
+    return 0;
+}
 //implement the functions here

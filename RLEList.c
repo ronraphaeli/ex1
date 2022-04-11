4cf5e0b719216c4b1f50aae1a1b3cf05af4c9d48
RotemShezaf <rotem.shezaf@gmail.com>
2022-04-11 18:39:00 +0300

    rotemFirstCommit

--- RLEList.c
+++ RLEList.c
@@ -1,7 +1,21 @@
 #include "RLEList.h"
 
-typedef struct {
+typedef struct list {
+    char data;
+    char* next;
+    int multiplier;
     //TODO: implement
 } RLEList_t;
+
+
+RLEList RLEListCreate() {
+
+
+};
+
+void RLEListDestroy(RLEList list) {
+
+};
+
 
 //implement the functions here

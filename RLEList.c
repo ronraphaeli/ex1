abba49b99a3a4edb3b286698d7c269e17a6a24b5
RotemShezaf <rotem.shezaf@gmail.com>
2022-04-12 13:03:45 +0300

    newStructEasyFunctions

--- RLEList.c
+++ RLEList.c
@@ -6,22 +6,29 @@ typedef struct  RLEList_t {
     struct RLEList_t * next;
     int multiplier;
     //TODO: implement
-} RLEList_t;
-
-
-
-
-void RLEListDestroy(RLEList list) {
-
-
 };
 
+void RLEListDestroy(RLEList list) {
+    while (list) {
+        RLEList toDelete = list;
+        printf("%c", list->data);
+        list = list->next;
+        free(toDelete);
+    }
+};
+
+RLEList RLEListCreate() {
+    RLEList list = malloc(sizeof(*list));
+    if (!list) return NULL;
+    list->next = NULL;
+    return list;
+}
+
 int main() {
-    RLEList list;
-    list = (RLEList)malloc(sizeof(RLEList_t));
-    list->data = 'c';
-    printf("%c", list->data);
-    free(list);
-    return 0;
+    RLEList list1 = RLEListCreate(), list2 = RLEListCreate();
+    list1->data = 'a';
+    list2->data = 'b';
+    list1->next = list2;
+    RLEListDestroy(list1);
 }
 //implement the functions here

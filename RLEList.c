2f5602e65b0dc831060b5140cef69f7bbf068300
RotemShezaf <rotem.shezaf@gmail.com>
2022-04-13 20:56:00 +0300

    rotemAppendFunc

--- RLEList.c
+++ RLEList.c
@@ -1,17 +1,18 @@
 #include "RLEList.h"
 #include <stdlib.h>
-
+#define HEAD 0
 
 struct  RLEList_t {
     char data;
     struct RLEList_t * next;
+    struct RLEList_t * last;
     int multiplier;
 };
 
 void RLEListDestroy(RLEList list) {
     while (list) {
         RLEList toDelete = list;
-        printf("%c", list->data);
+        //printf("%d\n", toDelete->multiplier);
         list = list->next;
         free(toDelete);
     }
@@ -20,15 +21,36 @@ void RLEListDestroy(RLEList list) {
 RLEList RLEListCreate() {
     RLEList list = malloc(sizeof(*list));
     if (!list) return NULL;
+    list->data = HEAD;
     list->next = NULL;
+    list->last = list;
     return list;
 }
 
+RLEListResult RLEListAppend(RLEList list, char value) {
+    if (!list || !list->last)
+        return RLE_LIST_NULL_ARGUMENT;
+    if (list->last->data == value) {
+        list->last->multiplier++;
+        return RLE_LIST_SUCCESS;
+    }
+    RLEList newLink = malloc(sizeof(*list));
+    if (!newLink) return RLE_LIST_OUT_OF_MEMORY;
+    newLink->next = NULL;
+    newLink->data = value;
+    newLink->multiplier = 1;
+    list->last->next = newLink;
+    list->last = newLink;
+    return  RLE_LIST_SUCCESS;
+}
+
 int main() {
-    RLEList list1 = RLEListCreate(), list2 = RLEListCreate();
-    list1->data = 'a';
-    list2->data = 'b';
-    list1->next = list2;
-    RLEListDestroy(list1);
+    RLEList list = RLEListCreate();
+    RLEListAppend(list, 'C');
+    RLEListAppend(list, 'C');
+    RLEListAppend(list, 'r');
+    RLEListAppend(list, 'r');
+    RLEListAppend(list, 'r');
+    RLEListDestroy(list);
 }
 //implement the functions here

f71b7a673ae074af3859f5516ad00f3746f1ce46
Ron Raphaeli <ronraphaeli@gmail.com>
2022-04-16 14:36:00 +0300

    rons functions

--- RLEList.c
+++ RLEList.c
@@ -21,7 +21,7 @@ void RLEListDestroy(RLEList list) {
     free(list);
     while (item) {
         RLEListItem toDelete = item;
-        printf("%d\n", toDelete->counter);
+        //printf("%d\n", toDelete->counter);
         item = item->next;
         free(toDelete);
     }
@@ -63,6 +63,91 @@ RLEListResult RLEListAppend(RLEList list, char value) {
     return  RLE_LIST_SUCCESS;
 }
 
+int RLEListSize(RLEList list) {
+    if (list == NULL)
+        return -1;
+    int count = 0;
+    RLEListItem item = list->head;
+    while (item) {
+        count= count + item->counter;
+        item = item->next;
+    }
+    return count;
+};
+
+RLEListResult RLEListRemove(RLEList list, int index) {
+    if (list == NULL)
+        return RLE_LIST_NULL_ARGUMENT;
+    int count = 0;
+    bool foundToDelete = false;
+    RLEListItem nodeToDelete = NULL;
+    RLEListItem previousNode = list->head;
+    RLEListItem item = list->head;
+    while (item) {
+        for (int i = 0; i < item->counter; i++) {
+            if (index == count) {
+                nodeToDelete = item;
+                foundToDelete = true;
+                break;
+            }
+            count++;
+        }
+        if (nodeToDelete) {
+            previousNode = item;
+            break;
+        }
+        item = item->next;
+    }
+    if (!foundToDelete && index >= count || index < 0) {
+        return RLE_LIST_INDEX_OUT_OF_BOUNDS;
+    }
+    if ((nodeToDelete->counter)>1)
+    {
+        nodeToDelete->counter = nodeToDelete->counter - 1;
+        return RLE_LIST_SUCCESS;
+    }
+    previousNode->next = nodeToDelete->next;
+    free(nodeToDelete);
+    return RLE_LIST_SUCCESS;
+}
+
+char RLEListGet(RLEList list, int index, RLEListResult* result) {
+    if (!list) {
+        if (result) {
+            *result = RLE_LIST_NULL_ARGUMENT;
+        }
+        return 0;
+    }
+    int count = 0;
+    RLEListItem nodeToRead = NULL;
+    RLEListItem item = list->head;
+    while (item) {
+        for (int i = 0; i < item->counter; i++) {
+            if (index == count) {
+                nodeToRead = item;
+                break;
+            }
+            count++;
+        }
+        if (nodeToRead) {
+            if (result)
+            {
+                *result = RLE_LIST_SUCCESS;
+            }
+            return nodeToRead->data;
+        }
+        item = item->next;
+    }
+    if (index >= count || index < 0) {
+        if (result)
+        {
+            *result = RLE_LIST_INDEX_OUT_OF_BOUNDS;
+        }
+        return 0;
+    }
+    return 0;
+}
+
 int main() {
     RLEList list = RLEListCreate();
     RLEListAppend(list, 'C');
@@ -77,7 +162,11 @@ int main() {
     RLEListAppend(list, 'b');
     RLEListAppend(list, 'b');
     RLEListAppend(list, 'b');
-
+    printf("%d\n", RLEListSize(list));
+    printf("%c\n", RLEListGet(list,5, NULL));
+    RLEListRemove(list,5);
+    printf("%d\n", RLEListSize(list));
+    printf("%c\n", RLEListGet(list, 5, NULL));
 
     RLEListDestroy(list);
 }
